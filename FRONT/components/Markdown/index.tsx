8955291fee1e7cdbaec46aed395cee8d7bddf03c
ZauJulio <zauhdf@gmail.com>
2022-12-29 17:33:05 -0300

    fix: markdown render

--- FRONT/components/Markdown/index.tsx
+++ FRONT/components/Markdown/index.tsx
@@ -1,36 +1,37 @@
 import { Container } from "@chakra-ui/react";
 
 import ReactMarkdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { Prism as SyntaxHighlighterRenderer } from "react-syntax-highlighter";
+import ChakraUIRenderer from "chakra-ui-markdown-renderer";
 
 export interface MarkdownContainerProps {
-  content: string;
+  conteudo: string;
 }
 
 export default function MarkdownContainer(props: MarkdownContainerProps) {
-  const { content } = props;
+  const { conteudo } = props;
+
+  const renderers = {
+    ...ChakraUIRenderer(),
+    code({ node, inline, className, children, ...props }: any) {
+      const match = /language-(\w+)/.exec(className || "");
+
+      return !inline && match ? (
+        <SyntaxHighlighterRenderer language={match[1]} {...props}>
+          {String(children).replace(/\n$/, "")}
+        </SyntaxHighlighterRenderer>
+      ) : (
+        <code className={className} {...props}>
+          {children}
+        </code>
+      );
+    },
+  };
 
   return (
     <Container>
-      <ReactMarkdown
-        components={{
-          code({ node, inline, className, children, style, ...props }) {
-            const match = /language-(\w+)/.exec(className || "");
-
-            return !inline && match ? (
-              <SyntaxHighlighter language={match[1]} {...props}>
-                {String(children).replace(/\n$/, "")}
-              </SyntaxHighlighter>
-            ) : (
-              <code className={className} {...props}>
-                {children}
-              </code>
-            );
-          },
-        }}
-        skipHtml
-      >
-        {content}
+      <ReactMarkdown components={renderers} skipHtml>
+        {conteudo}
       </ReactMarkdown>
     </Container>
   );
