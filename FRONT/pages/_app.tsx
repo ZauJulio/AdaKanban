65be798ad7c45590de1d6a739b24f9e20202a8d6
ZauJulio <zauhdf@gmail.com>
2022-12-29 17:15:08 -0300

    feat: add custom theme colors

--- FRONT/pages/_app.tsx
+++ FRONT/pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "theme"
 
 import { KanbanContextProvider, AuthContextProvider } from "@/contexts";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <AuthContextProvider>
         <KanbanContextProvider>
           <Component {...pageProps} />
---
+++ FRONT/theme/index.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    darkT: {
+      100: "#2121213b",
+    },
+    cyan: {
+      100: "#2C7A7B",
+      200: "#256D85"
+    },
+  },
+});
+
+export default theme;
